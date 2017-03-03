2582a257243fe2c1a3087957576f0187a7791538
amolthombre21 <amolthombre21@gmail.com>
2017-03-02 23:14:56 -0600

    docs(module-rest): add javadocs

--- rest/clearsky/src/main/java/com/amolthombre/clearsky/controller/WeatherReadingController.java
+++ rest/clearsky/src/main/java/com/amolthombre/clearsky/controller/WeatherReadingController.java
@@ -30,26 +30,58 @@ public class WeatherReadingController {
 		this.weatherReadingService = weatherReadingService;
 	}	
 	
+	/**
+	 * Handles request for creating a new weather record {@link WeatherReading}
+	 * 
+	 * @param weatherReading object of {@link WeatherReading}
+	 * @return persisted {@link WeatherReading} object
+	 */
 	@RequestMapping(method = RequestMethod.POST)
 	public WeatherReading createWeatherReading(@RequestBody WeatherReading weatherReading) {		
 		return weatherReadingService.create(weatherReading);
 	}	
 	
+	/**
+	 * Handles request for returning list of cities
+	 * 
+	 * @return {@link List} of {@link String} 
+	 */
 	@RequestMapping(method = RequestMethod.GET, value = URI.CITIES)
 	public List<String> getListOfCities() {
 		return weatherReadingService.listOfCities();
 	}
 	
+	/**
+	 * Handles request for returning weather for a given city
+	 * 
+	 * @param city The name of the city for which weather is requested
+	 * @return {@link WeatherReading} object
+	 */
 	@RequestMapping(method = RequestMethod.GET, params = CITY)
 	public WeatherReading getWeatherForCity(@RequestParam(CITY) String city) {
 		return weatherReadingService.weatherForGivenCity(city);
 	}
 	
+	/**
+	 * Returns specific weather property for a given city
+	 * 
+	 * @param city The name of the city for which weather property is requested
+	 * @param property weather property from humidity, temperature,pressure
+	 * @return specific weather property
+	 * @throws Exception when property is not from humidity, temperature,pressure
+	 */
 	@RequestMapping(method = RequestMethod.GET, params = {CITY, PROPERTY})
 	public int getWeatherPropertyForCity(@RequestParam("city") String city, @RequestParam(PROPERTY) String property) throws Exception {		
 		return weatherReadingService.getWeatherPropertyForCity(city, property);
 	}
 	
+	/**
+	 * Returns average weather forecast for a given city
+	 * 
+	 * @param city The name of the city for which average weather forecast is requested
+	 * @param grain grain is either hourly or daily
+	 * @return {@link List} of {@link AverageWeather} 
+	 */
 	@RequestMapping(method = RequestMethod.GET, params = {CITY, GRAIN})
 	public List<AverageWeather> getAverageForCity(@RequestParam(CITY) String city, @RequestParam(GRAIN) String grain) {
 		return weatherReadingService.getAverageWeatherForCity(city, grain);
--- rest/clearsky/src/main/java/com/amolthombre/clearsky/service/WeatherReadingService.java
+++ rest/clearsky/src/main/java/com/amolthombre/clearsky/service/WeatherReadingService.java
@@ -6,15 +6,54 @@ import com.amolthombre.clearsky.entity.AverageWeather;
 import com.amolthombre.clearsky.entity.WeatherReading;
 
 public interface WeatherReadingService {
+	/**
+	 * Creates a {@link WeatherReading} object and stores it into database.
+	 * 
+	 * @param weatherReading {@link WeatherReading} object 
+	 * @return {@link WeatherReading} object
+	 */
 	public WeatherReading create(WeatherReading weatherReading);
 	
+	/**
+	 * Returns list of unique cities
+	 * 
+	 * @return {@link List} of {@link String} 
+	 */
 	public List<String> listOfCities();
 	
+	/**
+	 * Returns {@link WeatherReading} for a given city
+	 * 
+	 * @param city The name of city for which weather is requested 
+	 * @return {@link WeatherReading} object
+	 */
 	public WeatherReading weatherForGivenCity(String city);
 	
+	/**
+	 * Returns specific property for a city
+	 * 
+	 * @param city The name of city for which weather property is requested 
+	 * @param property The name of the weather property
+	 * @return {@link WeatherReading} object
+	 */
 	public WeatherReading getPropertyForCity(String city, String property);
 	
+	/**
+	 * Returns specific property for a city
+	 * 
+	 * @param city The name of city for which weather property is requested
+	 * @param property property The name of the weather property
+	 * @return int weather property 
+	 * @throws Exception when property is not from humidity, pressure, and temperature
+	 */
 	public int getWeatherPropertyForCity(String city, String property) throws Exception;
 	
+	/**
+	 * Returns {@link List} of {@link AverageWeather} object which contains daily or hourly weather average 
+	 * 
+	 * @param city The name of city for which average weather is requested
+	 * @param grain Can be hourly or daily
+	 * @return {@link List} of {@link AverageWeather}
+	 */
 	public List<AverageWeather> getAverageWeatherForCity(String city, String grain);
 }
